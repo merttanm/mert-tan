9a467cc7e4b07dff45cfefebb01f92336856a3c9
merttanm <68250572+merttanm@users.noreply.github.com>
2025-09-14 00:00:46 +0300

    Implement smooth scroll for navigation links

--- js/loader.js
+++ js/loader.js
@@ -1,17 +1,17 @@
 console.log("Loader.js çalışıyor!");
 
 // Sağ için component listesi (her biri ayrı div id olacak)
-const rightComponents = ['about', 'projects', 'experience', 'contact'];
+const rightComponents = ['about', 'projects', 'experience', 'contact', 'blog'];
 
 // Sol için component listesi
-const leftComponents = ['sidebar', 'menu']; // components/sidebar.html vs.
+const leftComponents = ['sidebar' ]; // componßents/sidebar.html vs.
 
 // Component yükleme fonksiyonu
 async function loadComponent(name, targetId) {
     try {
         const response = await fetch(`components/${name}.html`);
         if (!response.ok) throw new Error(`Component bulunamadı: ${name}`);
-        const html = await response.text();
+        const html = (await response.text()).trim();
 
         // Her component kendi div içinde olacak şekilde ekleme
         let container = document.getElementById(name);
